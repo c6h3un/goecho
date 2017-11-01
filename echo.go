ab5054057ccebda41550bb38733326e7016f191c
Helen Chang <helen.chang@droi.com.tw>
2017-11-01 10:23:43 +0800

    add host

--- echo.go
+++ echo.go
@@ -6,7 +6,8 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "You are from %s!", r.RemoteAddr)
+	fmt.Fprintf(w, "You are from %s, ", r.RemoteAddr)
+	fmt.Fprintf(w, "This is %s.", r.Host)
 }
 
 func main() {
