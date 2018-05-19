3aaea9607715fb293fac2b6c508a5aecf0310625
Chu Chun Chang (Helen) <c6h3un@gmail.com>
2018-05-19 21:03:46 +0800

    add hostname info

--- echo.go
+++ echo.go
@@ -2,31 +2,33 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
-        "net"
+	"os"
 )
 
 // GetLocalIP returns the non loopback local IP of the host
 func GetLocalIP() string {
-    addrs, err := net.InterfaceAddrs()
-    if err != nil {
-        return ""
-    }
-    for _, address := range addrs {
-        // check the address type and if it is not a loopback the display it
-        if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-            if ipnet.IP.To4() != nil {
-                return ipnet.IP.String()
-            }
-        }
-    }
-    return ""
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	
+	hostname, _ := os.Hostname()
 	fmt.Fprintf(w, "You are from %s, ", r.RemoteAddr)
-	fmt.Fprintf(w, "This is %s.", GetLocalIP())
+	fmt.Fprintf(w, "this is %s ", GetLocalIP())
+	fmt.Fprintf(w, "(%s)", hostname)
 }
 
 func main() {
